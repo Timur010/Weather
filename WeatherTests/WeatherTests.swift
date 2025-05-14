02a3a308687e0a80512efb60e77ed4704fbf9890
Timur010 <duakar010@gmail.com>
2025-05-14 13:04:12 +0300

    Initial Commit

---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Timur Kadiev on 14.05.2025.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
